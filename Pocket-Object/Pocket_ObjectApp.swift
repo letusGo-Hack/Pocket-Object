06b89d298caf797aa737b0e6d209773162e3f4e7
youabledev <youable.framios@gmail.com>
2023-06-10 15:01:30 +0900

    DEL: 크래시 코드 삭제

--- Pocket-Object/Pocket_ObjectApp.swift
+++ Pocket-Object/Pocket_ObjectApp.swift
@@ -13,7 +13,7 @@ struct Pocket_ObjectApp: App {
     
     var body: some Scene {
         WindowGroup {
-            TestContentView()
+            ContentView()
                 .modelContainer(for: [Content.self])
         }
         .modelContainer(for: Item.self)
