f87418e814392b2ee65dd8364511d1aea2016119
eden.joon <eden.joon@kakaocorp.com>
2023-06-10 13:40:57 +0900

    update ObjectCapture

--- Pocket-Object/CameraView.swift
+++ Pocket-Object/CameraView.swift
@@ -9,57 +9,83 @@ import RealityKit
 import SwiftUI
 
 struct CapturePrimaryView: View {
-    @StateObject var single = CaptureManager.shared
-    
+//    @StateObject var single = CaptureManager.shared
+    @StateObject var session = ObjectCaptureSession()
     var onDismiss: () -> Void
     
     var body: some View {
         ZStack {
-            ObjectCaptureView(session: single.session)
-            switch single.session.state {
-            case .initializing:
-                EmptyView()
-                
-            case .ready:
+            ObjectCaptureView(session: session)
+            if case .ready = session.state {
                 VStack {
                     Text("Ready")
                     Spacer()
                     Button {
-                        single.session.startDetecting()
+                        session.startDetecting()
                     } label: {
                         Text("Start Detecting")
                     }
                 }
-               
-            case .detecting:
+            } else if case .detecting = session.state {
                 VStack {
                     Text("Detecting")
                     Button {
-                        single.session.startCapturing()
+                        session.startCapturing()
                     } label: {
                         Text("Start Capturing")
                     }
                 }
-
-            case .capturing:
-                Text("capture")
-                
-            case .finishing:
-                Text("Finishing")
-                
-            case .completed:
-                Text("completed")
-                //사용자가 object 정보를 저장하는 view가 present
-                
-            case .failed(let error):
-                Text(error.localizedDescription)
-                
-            @unknown default:
-                Text("unknown default")
             }
+//            switch single.session.state {
+//            case .initializing:
+//                EmptyView()
+//                
+//            case .ready:
+//                VStack {
+//                    Text("Ready")
+//                    Spacer()
+//                    Button {
+//                        single.session.startDetecting()
+//                    } label: {
+//                        Text("Start Detecting")
+//                    }
+//                }
+//               
+//            case .detecting:
+//  
+//
+//            case .capturing:
+//                Text("capture")
+//                
+//            case .finishing:
+//                Text("Finishing")
+//                
+//            case .completed:
+//                Text("completed")
+//                //사용자가 object 정보를 저장하는 view가 present
+//                
+//            case .failed(let error):
+//                Text(error.localizedDescription)
+//                
+//            @unknown default:
+//                Text("unknown default")
+//            }
         }
+        .onAppear(perform: {
+            var configuration = ObjectCaptureSession.Configuration()
+            configuration.checkpointDirectory = getDocumentsDirectory().appendingPathComponent("Snapshots/")
+            configuration.isOverCaptureEnabled = true
+            let url: URL = getDocumentsDirectory().appendingPathComponent("Images/")
+            print(url.absoluteString)
+            session.start(imagesDirectory: url, configuration: configuration)
+        })
         .onDisappear(perform: {
             onDismiss()
         })
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
