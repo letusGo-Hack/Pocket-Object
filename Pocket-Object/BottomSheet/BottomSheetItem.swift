f9ace334dd67809d8379fde8d37fe83608d3a985
youabledev <youable.framios@gmail.com>
2023-06-10 14:39:10 +0900

    ADD: SFSymbol animate 버튼 추가

--- Pocket-Object/BottomSheet/BottomSheetItem.swift
+++ Pocket-Object/BottomSheet/BottomSheetItem.swift
@@ -8,27 +8,36 @@
 import SwiftUI
 
 struct ListItemView: View {
+    let dateformat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY년 M월 d일"
+        return formatter
+    }()
+    
+    @State var bounceValue: Int = 0
     var content: Content
     
     var body: some View {
         HStack {
             VStack {
-                Text("\(content.date)")
+                Text("\(content.date, formatter: dateformat)")
                 Text("\(content.title)")
                 
                 Spacer()
                 
                 HStack {
                     Button {
-                        
+                        content.bookmark.toggle()
                     } label: {
-                        Text("bookmark")
+                        Image(systemName: content.bookmark ? "bookmark.fill" : "bookmark")
+                            .contentTransition(.symbolEffect(.replace.offUp))
                     }
                     
                     Button {
-                        
+                        bounceValue += 1
                     } label: {
-                        Text("share")
+                        Image(systemName: "square.and.arrow.up")
+                            .symbolEffect(.bounce, value: bounceValue)
                     }
                 }
                 
