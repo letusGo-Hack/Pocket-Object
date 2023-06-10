f255f1fc35cafcdaaf050c1f3717c136711b2471
youabledev <youable.framios@gmail.com>
2023-06-10 13:53:29 +0900

    ADD: BottomSheetView

---
+++ Pocket-Object/BottomSheet/BottomSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSheetView.swift
+//  Pocket-Object
+//
+//  Created by ZUMIN YOU on 2023/06/10.
+//
+
+import SwiftUI
+
+struct BottomSheetView: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+#Preview {
+    BottomSheetView()
+}
